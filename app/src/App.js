07b225549d419bff862163c42db4cc805a3c8c1d
angelsmart86 <angelsmart86@gmail.com>
2024-10-16 23:49:14 -0300

    Update project files

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'; // si quieres usar un estilo global
+import LoginRegister from './components/LoginRegister';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginRegister />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
