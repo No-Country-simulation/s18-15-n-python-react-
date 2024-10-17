3b3a335a21209aea57aab2257f5bc9d8bd89d886
Moises2024 <agustinprato56@gmail.com>
2024-10-16 22:08:40 -0400

    borrar o no, primer adelanto

---
+++ mensaje1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Borrar_o_No from './components/Borrar_o_No'
+
+function App() {
+
+  return (
+    <div>
+      <Borrar_o_No />
+    </div>
+  )
+}
+
+export default App
---
+++ mensaje1/src/components/Borrar_o_No.jsx
@@ -0,0 +1,16 @@
+function borrar() {
+    return (
+        <>
+            <div>
+                <div>
+                    <img src="react.svg" alt="imagen" />
+                </div>
+                <h2>Estas seguro de que quieres eliminar la tarea?</h2>
+                <button>No, mantener</button>
+                <button>Eliminar</button>
+            </div>
+        </>
+    )
+}
+
+export default borrar;
