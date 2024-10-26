4ee23789adddcfa848ca41b81cfa63380e383395
Martin <angelsmart86@gmail.com>
2024-10-26 04:18:20 -0300

    PlanificaPlus

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.querySelector('.btn-subscribe').addEventListener('click', function (e) {
+    e.preventDefault();
+    alert('¡Gracias por tu interés! Pronto podrás disfrutar de todas las características premium.');
+});
+
+
