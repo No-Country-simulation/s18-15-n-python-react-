077a7705fdc2baa5a9dc2ef7345deda62938e26f
Random-Dev143 <facundo.rua90@gmail.com>
2024-10-27 20:06:04 -0300

    user_update route add

--- Back/auth/__init__.py
+++ Back/auth/__init__.py
@@ -2,13 +2,16 @@ from fastapi import APIRouter
 from .register_route import router as register_router
 from .oauth import router as oauth_router
 from .login_route import router as login_router
-from .delete_user import router as delete_user
+from .delete_user import router as delete_user_router
+from .user_update import router as  user_update_router
+
 
 # Crear un router combinado
 router = APIRouter()
 router.include_router(register_router)
 router.include_router(oauth_router)
 router.include_router(login_router)
-router.include_router(delete_user)
+router.include_router(delete_user_router)
+router.include_router(user_update_router)
 
 __all__ = ["router"] 
